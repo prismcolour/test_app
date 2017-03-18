78425d7c352558d1d906e276b515149d836a7874
prism colour <prismcolour@icloud.com>
2017-03-18 17:39:28 +0000

    Add prefix to default route and add index links

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'default#index'
+  root 'default#index', as: :landing
 end
